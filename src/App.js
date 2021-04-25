48d6c24d60b39d2279e73c39d736b03eafb5b2b2
Mohamed Abd El-Monem <mo.hagag27@gmail.com>
2021-04-25 07:36:07 +0200

    base added

--- src/App.js
+++ src/App.js
@@ -35,6 +35,12 @@ export const App = () => {
     <Layout productsCount={cartCount}>
       <Switch>
         <Route
+          path="/:product"
+          render={(props) => (
+            <Product {...props} addProduct={incrementCartCount} />
+          )}
+        />
+        <Route
           path="/"
           exact
           render={(props) => (
@@ -44,14 +50,6 @@ export const App = () => {
               addProduct={incrementCartCount}
               deleteProduct={decrementCartCount}
             />
-          )}
-        />
-
-        <Route
-          path="/:product"
-          exact
-          render={(props) => (
-            <Product {...props} addProduct={incrementCartCount} />
           )}
         />
       </Switch>
